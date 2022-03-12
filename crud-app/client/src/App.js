ddc01518bc9ebda10cae08a52391f36d4bdebfd3
Szymon Szaminko <sz.szaminko@gmail.com>
2022-03-12 22:22:08 +0100

    Styling whole form

--- crud-app/client/src/App.js
+++ crud-app/client/src/App.js
@@ -3,23 +3,36 @@ import "./App.css";
 function App() {
   return (
     <div className='App'>
-      <div className='information'>
-        <label>Name: </label>
-        <input type='text'></input>
+      <div className='info-form'>
+        <h1>Employee Register</h1>
+        <div className='information'>
+          <div className='label-input'>
+            <label>Name: </label>
+            <input type='text'></input>
+          </div>
 
-        <label>Age: </label>
-        <input type='number'></input>
+          <div className='label-input'>
+            <label>Age: </label>
+            <input type='number'></input>
+          </div>
 
-        <label>Country: </label>
-        <input type='text'></input>
+          <div className='label-input'>
+            <label>Country: </label>
+            <input type='text'></input>
+          </div>
 
-        <label>Position: </label>
-        <input type='text'></input>
+          <div className='label-input'>
+            <label>Position: </label>
+            <input type='text'></input>
+          </div>
 
-        <label>Wage (year): </label>
-        <input type='number'></input>
+          <div className='label-input'>
+            <label>Wage (year): </label>
+            <input type='number'></input>
+          </div>
 
-        <button>Add Employee</button>
+          <button>Add Employee</button>
+        </div>
       </div>
     </div>
   );
