1139aaad59b529bbf4f34c642849a09ccd20aa0d
Szymon Szaminko <sz.szaminko@gmail.com>
2022-03-14 00:45:56 +0100

    Styling ShowEmployee component and remove scrollbar

--- crud-app/client/src/components/ShowEmployee.js
+++ crud-app/client/src/components/ShowEmployee.js
@@ -9,14 +9,32 @@ function ShowEmployee() {
   const getEmployees = () => {
     Axios.get("http://localhost:3001/employees").then(response => {
       console.log(response.data);
+      setEmployeeList(response.data);
     });
   };
 
   return (
-    <div className='info-form'>
+    <div className='employee-list'>
       <h1>Show Employees</h1>
       <div className='show-employees-btn'>
         <button onClick={getEmployees}>Show Employess</button>
+        <div className='list'>
+          {employeeList.map((value, key) => {
+            return (
+              <div className='employee-card'>
+                <div className='card-part1'>
+                  <div key={value.name}>Name: {value.name}</div>
+                  <div key={value.age}>Age: {value.age}</div>
+                </div>
+                <div className='card-part2'>
+                  <div key={value.country}>Country: {value.country}</div>
+                  <div key={value.position}>Position: {value.position}</div>
+                  <div key={value.wage}>Wage: {value.wage}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
