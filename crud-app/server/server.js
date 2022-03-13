8802c9cfd017f76c147c5f5b457aa01b1d9eb3bd
Szymon Szaminko <sz.szaminko@gmail.com>
2022-03-13 11:35:21 +0100

    Define a endpoint post and connection to db

--- crud-app/server/server.js
+++ crud-app/server/server.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
-app.listen(3001, () => {
-  console.log("Server is working on port 3001");
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "", //or password: 'password'
+  database: "employeeSystem",
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name,age,country,position,wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+
+  app.listen(3001, () => {
+    console.log("Server is working on port 3001");
+  });
 });
